4041a9d3d61a6695369c1a12d76d3515e25f670f
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-11-04 17:01:59 +0100

    linktesting

--- 18Host/TDT4120/AtomProjectFolder/Project1/Main.jl
+++ 18Host/TDT4120/AtomProjectFolder/Project1/Main.jl
@@ -6,7 +6,7 @@ imp = ImageProcessing
 function main()
 	println("Starting")
 	# img = imp.loadImg("res/mazes/maze10.png")
-	@time img = imp.loadFloatImg("res/img.png")
+	@time img = imp.loadFloatImg("res/img2.png")
 
 	@time imp.sobell!(img)
 
