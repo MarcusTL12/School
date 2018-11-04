1105a0382de14bd60307b2c960f825f8587b3867
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-11-04 02:18:15 +0100

    fix

--- 18Host/TDT4120/AtomProjectFolder/Project1/Main.jl
+++ 18Host/TDT4120/AtomProjectFolder/Project1/Main.jl
@@ -6,36 +6,9 @@ imp = ImageProcessing
 function main()
 	println("Starting")
 	# img = imp.loadImg("res/mazes/maze10.png")
-	@time img = imp.loadFloatImg("res/img2.png")
-	kernel = reshape(Float32[
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-		1 0 0 0 0 0 0	0 0 0 0 0 0 1	0 0 0 0 0 0 1
-	], (7, 7, 3))
-	# kernel = Float32[
-	# 	1 2 3 4 3 2 1;
-	# 	2 3 4 5 4 3 2;
-	# 	3 4 5 6 5 4 3;
-	# 	4 5 6 7 6 5 4;
-	# 	3 4 5 6 5 4 3;
-	# 	2 3 4 5 4 3 2;
-	# 	1 2 3 4 3 2 1
-	# ]
+	@time img = imp.loadFloatImg("res/img.png")
 
-	kernel[:, :, :] ./= sum(kernel[:, :, 2])
-	# println(kernel[:, :, 1])
-	# println(kernel[:])
-
-	# @time imp.simple_gaussian!(img)
-	# @time imp.grayscale!(img)
-	@time imp.apply_kernel!(img, kernel)
-	# @time img ./= sum(kernel[1, :, :])
-	# @time img ./= sum(kernel)
-	# img ./= sum(kernel[1, :, :])
+	@time imp.sobell!(img)
 
 	@time imp.writeImg("res/temp1.png", img)
 end
--- 18Host/TDT4120/AtomProjectFolder/Project1/_Start.bat
+++ 18Host/TDT4120/AtomProjectFolder/Project1/_Start.bat
@@ -1,3 +1,3 @@
 @echo off
-REM set JULIA_NUM_THREADS=12
+set JULIA_NUM_THREADS=12
 julia Main.jl
