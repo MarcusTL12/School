f0bd4c18b9679ac5121f4b9ee304004fcb881730
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-11-06 12:22:08 +0100

    _

--- 18Host/TDT4120/AtomProjectFolder/Project1/Main.jl
+++ 18Host/TDT4120/AtomProjectFolder/Project1/Main.jl
@@ -6,16 +6,14 @@ imp = ImageProcessing
 function main()
 	println("Starting")
 	# img = imp.loadImg("res/mazes/maze10.png")
-<<<<<<< HEAD
+
 	@time img = imp.loadFloatImg("res/img2.png")
 
 	@time imp.simple_blur!(img)
 	@time imp.sobell!(img)
-=======
 	@time img = imp.loadFloatImg("res/PigBG.png")
 
 	f(x, y) = sin(pi * x * y)^2
->>>>>>> cacb30eb4e64528263ba187e59ffd4dfa1c29640
 
 	# @time img[1, :, :] .= f.(img[1, :, :], 6 + 10)
 	# @time img[2, :, :] .= f.(img[2, :, :], 100)
