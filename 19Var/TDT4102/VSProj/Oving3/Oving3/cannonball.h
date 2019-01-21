34f16cf83941c7f8641309fc1fed577df099c7bb
MarcusTL12 <marcuspaafjellet@gmail.com>
2019-01-21 23:20:39 +0100

    Started Øving 3 (TDT4102) and tested Weave in latex

---
+++ 19Var/TDT4102/VSProj/Oving3/Oving3/cannonball.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//a)
+double acclY();
+
+//b)
+double velY(double initVelocityY, double time);
+
+//c)
+double posX(double initPosition, double initVelocity);
+double posY(double initPosition, double initVelocity);
+
+//d)
+void printTime(double time);
+
+//e)
+double flightTime(double initVelocity);
---
+++ 19Var/TDT4102/VSProj/Oving3/Oving3/main.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+
+int main() {
+	cout << "Hallais\n";
+
+	keep_window_open();
+}
+
